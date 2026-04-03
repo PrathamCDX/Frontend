9bc591af3fea38c288da652fb0b9e937be722111
Arijit Ganguly <aganguly158@gmail.com>
2026-04-03 15:03:15 +0530

    #fix: fixed the redux store configuartion

--- src/components/ReduxProvider.tsx
+++ src/components/ReduxProvider.tsx
@@ -1,8 +1,8 @@
-'use client';
-import  { store } from "@/lib/store.config";
-import { ReactNode } from "react";
-import { Provider } from "react-redux";
+// 'use client';
+// import  { store } from "@/lib/store.config";
+// import { ReactNode } from "react";
+// import { Provider } from "react-redux";
 
-export default function ReduxProvider({ children }: { children: ReactNode }) {
-  return <Provider store={store}>{children}</Provider>;
-}
+// export default function ReduxProvider({ children }: { children: ReactNode }) {
+//   return <Provider store={store}>{children}</Provider>;
+// }
